8d5f31d783ec9ab53c764e512976c6e0c87577ad
whjin <1414848321@qq.com>
2022-04-21 21:33:21 +0800

    新建gh-pages分支

---
+++ i18n/en-US/dict.js
@@ -0,0 +1,39 @@
+module.exports = {
+    experience_type: {
+        "internship": "(internship)"
+    },
+    skill_type: {
+        "frontend": "Frontend Development",
+        "backend": "Backend Development",
+        "practice": "Practice Development",
+        "miscellaneous": "Miscellaneous",
+    },
+    education_type: {
+        "undergraduate": "(Bachelor's degree)"
+    },
+    link_type: {
+        "homepage": "Homepage",
+        "segmentfault": "SegmentFault",
+        "repo": "Source",
+        "pdf": "PDF"
+    },
+    section_names: {
+        "education": {
+            "ch": "Education",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "Language",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "Experience",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "Skills",
+            "en": "Skills"
+        }
+    },
+    lt_ie8_comment: "You are using IE8 or lower version of IE, which is not supported."
+};
---
+++ i18n/zh-CN/dict.js
@@ -0,0 +1,46 @@
+module.exports = {
+  experience_type: {
+    "internship": ""
+  },
+  skill_type: {
+    "frontend": "Web 前端",
+    "backend": "Web 后端",
+    "practice": "前端工程实践",
+    "miscellaneous": "其他",
+  },
+  education_type: {
+    "undergraduate": "（本科）"
+  },
+  link_type: {
+    "homepage": "技术博客",
+    "segmentfault": "技术论坛",
+    "repo": "源代码",
+    "pdf": "PDF 下载"
+  },
+  section_names: {
+    "education": {
+      "ch": "教育经历",
+      "en": "Education"
+    },
+    "language": {
+      "ch": "语言水平",
+      "en": "Language"
+    },
+    "experience": {
+      "ch": "项目与工作经验",
+      "en": "Experience"
+    },
+    "skills": {
+      "ch": "技能",
+      "en": "Skills"
+    }
+  },
+  lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~",
+  dict: {
+    source_code: "源代码",
+    demo: "DEMO"
+  },
+  project_type: {
+    "personal": "开发项目"
+  }
+};
